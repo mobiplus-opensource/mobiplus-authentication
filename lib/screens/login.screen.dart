6c71fa42838f9ab92cdb586aa337572e02f94e9e
Wellington Felisberto <wellington.silva@mobiplus.com.br>
2022-01-21 11:16:09 -0300

    adding components to login screen

--- lib/screens/login.screen.dart
+++ lib/screens/login.screen.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget{
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() {
+    return LoginScreenStates();
+  }
+}
+
+class LoginScreenStates extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: LayoutBuilder(builder: (_, constraints) {
+          return Column(
+            children: [
+              Flexible(
+                child: Container(
+                  color: Colors.white,
+                    child: Expanded(
+                      child: Image(
+                        image: AssetImage('lib/assets/images/mobiplus_logo.png'),
+                      ),
+                    )),
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: ElevatedButtonTheme(
+                      data: ElevatedButtonThemeData(style: ButtonStyle()),
+                      child: ElevatedButton(
+                        onPressed: () async {},
+                        child: Text('Continuar com Google'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: ElevatedButtonTheme(
+                      data: ElevatedButtonThemeData(style: ButtonStyle()),
+                      child: ElevatedButton(
+                        onPressed: () async {},
+                        child: Text('Continuar com Apple'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+        }),
+      ),
+    );
   }
-
 }
