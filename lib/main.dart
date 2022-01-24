c07be8d662b5a31226d9a7c96564d3a5c2cc8b63
Wellington Felisberto <wellington.silva@mobiplus.com.br>
2022-01-24 11:16:16 -0300

    adding and configuring google_sign_in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobiplus_authentication/screens/home.screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final teste = Firebase.app();
   runApp(const MyApp());
 }
 
