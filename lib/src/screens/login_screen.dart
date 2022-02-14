a09a89a7edc6e48a5444e86d6c423fdb3458e073
Wellington Felisberto <wellington.silva@mobiplus.com.br>
2022-02-14 15:33:59 -0300

    remaking login screen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -22,46 +22,70 @@ class LoginScreenStates extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.blueGrey[800],
         body: LayoutBuilder(builder: (_, constraints) {
-          return Container(
-            margin: const EdgeInsets.only(top: 50),
+          return Padding(
+            padding: const EdgeInsets.all(32),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                      padding: const EdgeInsets.only(top: 20),
-                      alignment: Alignment.center,
-                      child: widget.image),
+                const Spacer(),
+                Container(
+                    padding: const EdgeInsets.only(top: 20),
+                    alignment: Alignment.center,
+                    child: widget.image),
+                const Spacer(),
+                const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Olá, \nSeja bem-vindo novamente',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    )),
+                const SizedBox(height: 8),
+                const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Faça login em sua\nconta para continuar ',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    )),
+                const Spacer(),
+                const SizedBox(
+                  height: 40,
                 ),
-                Flexible(
-                  flex: 2,
-                  child: Column(
-                    children: [
-                      Container(
-                        width: constraints.maxWidth - 120,
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: ElevatedButtonTheme(
-                          data: ElevatedButtonThemeData(
-                              style: widget.buttonStyle),
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              final provider =
-                                  Provider.of<GoogleSignInProvider>(context,
-                                      listen: false);
-                              provider.googleLogin(context);
-                            },
-                            child: Text(
-                              widget.buttonText,
-                              style: widget.buttonTextStyle,
-                            ),
+                Column(
+                  children: [
+                    Container(
+                      width: constraints.maxWidth - 120,
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: ElevatedButtonTheme(
+                        data:
+                            ElevatedButtonThemeData(style: widget.buttonStyle),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            final provider = Provider.of<GoogleSignInProvider>(
+                                context,
+                                listen: false);
+                            provider.googleLogin(context);
+                          },
+                          child: Text(
+                            widget.buttonText,
+                            style: widget.buttonTextStyle,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
+                RichText(
+                    text:
+                        const TextSpan(text: 'Já possui uma conta?', children: [
+                  TextSpan(
+                      text: 'Log in',
+                      style: TextStyle(decoration: TextDecoration.underline))
+                ])),
               ],
             ),
           );
